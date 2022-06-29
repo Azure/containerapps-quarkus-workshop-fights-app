5c9c6f1be961969dbc3774130bcdce7c072c37cf
Antonio Goncalves <antonio.goncalves@gmail.com>
2022-06-29 14:24:53 +0200

    Refactor resource name

--- src/test/java/com/azure/containerapps/quarkus/workshop/superheroes/fight/FightResourceIT.java
+++ src/test/java/com/azure/containerapps/quarkus/workshop/superheroes/fight/FightResourceIT.java
@@ -3,7 +3,7 @@ package com.azure.containerapps.quarkus.workshop.superheroes.fight;
 import io.quarkus.test.junit.QuarkusIntegrationTest;
 
 @QuarkusIntegrationTest
-public class GreetingResourceIT extends GreetingResourceTest {
+public class FightResourceIT extends FightResourceTest {
 
     // Execute the same tests but in native mode.
 }
